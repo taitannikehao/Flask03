76a7ba0ef5d13e4a4d95df1696259921f9bbba4b
taitannikehao <1611747933@qq.com>
2019-09-06 22:54:30 +0800

    在test01 中创建一个视图函数

--- test01_template.py
+++ test01_template.py
@@ -4,9 +4,11 @@ from flask  import Flask,config
 from flask_script import  Manager
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 manage = Manager(app)
 
-
+@app.route('/')
+def index():
+    return 'index'
 if __name__ == '__main__':
     manage.run()
